a0123d2f513e4d3bd7920bb1282fc6b6ad0df170
Rose Francis <rosennabs@gmail.com>
2024-03-05 15:58:47 -0700

    setup and config tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="flex justify-center items-center mt-10">
+      <h1 className="text-3xl text-amber-700 uppercase font-bold font-sans-serif"> Welcome to Appetive! </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      amber: colors.amber
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  plugins: [],
+};
+
