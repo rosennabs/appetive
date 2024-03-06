87cfcf5a445c6b05e05f2e814aab37c3411c9501
maddie <lee.maddie@gmail.com>
2024-03-06 14:48:48 -0500

    Add inputs for values from ERD

--- frontend/src/components/RecipeForm.jsx
+++ frontend/src/components/RecipeForm.jsx
@@ -12,6 +12,36 @@ function RecipeForm() {
           name="name"
           type="text"
         />
+        <label htmlFor="cuisine">Cuisine</label>
+        <input
+          id="cuisine"
+          name="cuisine"
+          type="text"
+        />
+        <label htmlFor="diet">Diet</label>
+        <input
+          id="diet"
+          name="diet"
+          type="text"
+        />
+        <label htmlFor="prep_time">Prep Time</label>
+        <input
+          id="prep_time"
+          name="prep_time"
+          type="text"
+        />
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          id="instructions"
+          name="instructions"
+        />
+        <label htmlFor="tags">Tags</label>
+        <input
+          id="tags"
+          name="tags"
+          type="text"
+        />
+
         <button className="text-white bg-black" type="submit">Submit</button>
       </form>
     </div>
