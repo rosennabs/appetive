50222c8b02261c453aec9beee52be798fea0ded2
maddie <lee.maddie@gmail.com>
2024-03-06 14:42:36 -0500

    Create basic HTML form

--- frontend/src/components/RecipeForm.jsx
+++ frontend/src/components/RecipeForm.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { useFormik } from "formik";
 
 function RecipeForm() {
+
   return (
-    <h1>Recipe Form</h1>
+    <div>
+      <form onSubmit={(event) => {event.preventDefault()}}>
+        <label htmlFor="name">Recipe Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+        />
+        <button className="text-white bg-black" type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
