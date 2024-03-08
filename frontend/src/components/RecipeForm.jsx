1960e5387c803eabfdc36808a26454dd796e0459
maddie <lee.maddie@gmail.com>
2024-03-07 22:56:58 -0500

    Use Formik Form component

--- frontend/src/components/RecipeForm.jsx
+++ frontend/src/components/RecipeForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FieldArray, Formik } from "formik";
+import { Formik, Form, FieldArray } from "formik";
 
 function RecipeForm() {
   const emptyIngredient = {
@@ -22,85 +22,81 @@ function RecipeForm() {
         console.log(values);
       }}
     >
-      {({ values }) => (
-    <div>
-      <form className="w-full max-w-lg">
-        <div className="flex flex-wrap">
-          <div className="w-full">
-            <label className="block" htmlFor="name">Recipe Name</label>
-            <input
-              id="name"
-              name="name"
-              type="text"
-              className="border"
-              />
-          </div>
-          <div className="w-full">
-            <label className="block" htmlFor="cuisine">Cuisine</label>
-            { /* to update with mock data */}
-            <select id="diet" name="diet"
-            className="border">
-              <option value="French">French</option>
-              <option value="Jamaican">Jamaican</option>
-            </select>
-          </div>
-          <div className="w-full">
-            <label className="block" htmlFor="diet">Diet</label>
-            { /* to update with mock data */}
-            <select id="diet" name="diet"
-            className="border">
-              <option value="Keto">Keto</option>
-              <option value="Vegetarian">Vegetarian</option>
-            </select>
-          </div>
-          <div className="w-full">
-          <label className="block" htmlFor="prep_time">Prep Time</label>
+    {({ values }) => (
+     <Form>
+        <div className="w-full">
+          <label className="block" htmlFor="name">Recipe Name</label>
           <input
-            id="prep_time"
-            name="prep_time"
+            id="name"
+            name="name"
             type="text"
             className="border"
             />
-          </div>
-
-          <div className="w-full">
-            <label className="block" htmlFor="ingredient">Ingredients</label>
-            <FieldArray name="ingredients">
-              {({ push, remove }) => {
-                <>
-                {values.ingredients.map((_ingredient, index) => {
-                  return (
-                    { /* Ingredient*/ }
-                  )
-                })}
-                <button>+</button>
-                </>
-              }}
-            </FieldArray>
-          </div>
-
-          <div className="w-full">
-            <label className="block" htmlFor="instructions">Instructions</label>
-            <textarea
-              id="instructions"
-              name="instructions"
-              className="w-full border"
-              />
-          </div>
-          <div className="w-full">
-          <label className="block" htmlFor="tags">Tags</label>
-          <input
-            id="tags"
-            name="tags"
-            type="text"
-            className="border"
-            />
-          </div>
-
-          <button className="text-white bg-black" type="submit">Submit</button>
         </div>
-      </form>
-    </div>
+        <div className="w-full">
+          <label className="block" htmlFor="cuisine">Cuisine</label>
+          { /* to update with mock data */}
+          <select id="diet" name="diet"
+          className="border">
+            <option value="French">French</option>
+            <option value="Jamaican">Jamaican</option>
+          </select>
+        </div>
+        <div className="w-full">
+          <label className="block" htmlFor="diet">Diet</label>
+          { /* to update with mock data */}
+          <select id="diet" name="diet"
+          className="border">
+            <option value="Keto">Keto</option>
+            <option value="Vegetarian">Vegetarian</option>
+          </select>
+        </div>
+        <div className="w-full">
+        <label className="block" htmlFor="prep_time">Prep Time</label>
+        <input
+          id="prep_time"
+          name="prep_time"
+          type="text"
+          className="border"
+          />
+        </div>
+
+        <div className="w-full">
+          <label className="block" htmlFor="ingredient">Ingredients</label>
+          <FieldArray name="ingredients">
+            {({ push, remove }) => {
+              <>
+              {values.ingredients.map((_ingredient, index) => {
+                return (
+                  { /* Ingredient*/ }
+                )
+              })}
+              <button>+</button>
+              </>
+            }}
+          </FieldArray>
+        </div>
+
+        <div className="w-full">
+          <label className="block" htmlFor="instructions">Instructions</label>
+          <textarea
+            id="instructions"
+            name="instructions"
+            className="w-full border"
+            />
+        </div>
+        <div className="w-full">
+        <label className="block" htmlFor="tags">Tags</label>
+        <input
+          id="tags"
+          name="tags"
+          type="text"
+          className="border"
+          />
+        </div>
+
+        <button className="text-white bg-black" type="submit">Submit</button>
+       </Form>
       )}
     </Formik>
   );
