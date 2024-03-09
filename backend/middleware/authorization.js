b43350c52a24894c0bd96fa4321bf8e2dcdeb91f
Mia Dinh <miadinh38@gmail.com>
2024-03-09 13:01:51 -0500

    Call next()

--- backend/middleware/authorization.js
+++ backend/middleware/authorization.js
@@ -13,6 +13,7 @@ module.exports = async (req,res,next) => {
     const payload =  jwt.verify(jwtToken, process.env.jwtSecret)
 
     req.user = payload.user;
+    next();
     
   } catch (error) {
     console.error(error.message);
