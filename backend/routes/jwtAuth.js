3e44fc720fd7a1b9c3c2359ad6a37e2cece64a24
Mia Dinh <miadinh38@gmail.com>
2024-03-09 12:13:58 -0500

    Create register and login routes with jwt token

--- backend/routes/jwtAuth.js
+++ backend/routes/jwtAuth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const db = require("../db/connection");
 const bcrypt = require("bcrypt");
+const jwtGenerator = require("../utils/jwtGenerator")
 
 //Registering route
 
@@ -27,14 +28,46 @@ router.post("/register", async(req,res) => {
     //4. Enter the new user into our db
     const newUser = await db.query("INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *", [name, email, bcryptPassword]);
 
-    res.json(newUser.rows[0]);
+    // res.json(newUser.rows[0]);
 
     //5. Generating our jwt token
+
+    const token = jwtGenerator(newUser.rows[0].id);
+    res.json({ token });
     
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
   }
 });
+
+// Login route
+router.post("/login", async(req,res) => {
+  try {
+    // 1. Destructure req.body
+    const { email, password } = req.body
+    // 2. Check if user doesn't exit, throw error
+    const user = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+    // res.json(user.rows[0]);
+    if (user.rows.length === 0) {
+      return res.status(401).send("Password or email is incorrect")
+    }
+
+    // 3. Check if the password if the same with db password
+    const validPassword = await bcrypt.compare(password, user.rows[0].password);
+    if (!validPassword) {
+      res.status(401).send("Password or email is incorrect");
+    }
+
+    // 4. Give the jwt token
+    const token = jwtGenerator(user.rows[0].id)
+    res.json({ token })
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");    
+  }
+
+})
 
 module.exports = router;
