361a00f10735bc7780160a98349e5b2f7b4b22ba
Mia Dinh <miadinh38@gmail.com>
2024-03-09 15:32:39 -0500

    Create login page and add styling

---
+++ frontend/src/components/login.jsx
@@ -0,0 +1,51 @@
+import React, { Fragment } from "react";
+
+function Login() {
+  return (
+    <Fragment>
+      <h1 className="text-5xl py-4">Login</h1>
+      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+
+        <button
+          className="border-black border-2 rounded-2xl py-2 px-4"
+          type="button"
+        >
+          Login
+        </button>
+      </form>
+      
+    </Fragment>
+  );
+}
+
+export default Login;
