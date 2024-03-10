17c81294505b1dd6256281e7c965cbb659a4c6fa
hisbanshiraz <hisbanbardai@gmail.com>
2024-03-10 11:43:50 -0600

    Add schema of recipes_ingredients table and add mock data into it

---
+++ backend/db/seeds/recipes_ingredients.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+  recipes_ingredients (recipe_id, ingredient_id, measurement)
+VALUES
+  (1, 1, '50g');
+
+INSERT INTO
+  recipes_ingredients (recipe_id, ingredient_id, measurement)
+VALUES
+  (2, 2, '150g');
