9cf89368335d097eb8af34714b5cf60eaade8a64
Mia Dinh <miadinh38@gmail.com>
2024-03-11 01:22:14 -0400

    Create 'About Us' Page

---
+++ frontend/src/components/NavBar/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+    <h1 className="text-5xl py-10 mx-auto text-center">
+      About Us
+    </h1>
+    </>
+  )
+}
+
+export default About;
