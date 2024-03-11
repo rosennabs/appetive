214fcfc225663f98d7550e24b342ce8a865bfc7c
hisbanshiraz <hisbanbardai@gmail.com>
2024-03-11 16:51:20 -0600

    Add useEffect hook to fetch recipe data from database

--- frontend/src/components/RecipeDetails.jsx
+++ frontend/src/components/RecipeDetails.jsx
@@ -1,7 +1,25 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const RecipeDetails = function ({ id }) {
   const [recipeDetails, setRecipeDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchRecipe = async function () {
+      try {
+        // Fetch recipe details
+        const recipeResponse = await axios.get(
+          `http://localhost:3000/api/recipes/5`
+        );
+        const recipeData = recipeResponse.data;
+        console.log(recipeData);
+      } catch (error) {
+        console.error("Error", error.message);
+      }
+    };
+
+    fetchRecipe();
+  }, [id]);
 
   return <h1>Recipe Details Page</h1>;
 };
