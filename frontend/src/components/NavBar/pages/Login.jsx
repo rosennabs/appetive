a479a5ae586f8fe5b62a3841d4148d3602689345
Mia Dinh <miadinh38@gmail.com>
2024-03-11 00:37:34 -0400

    Move Login and Register to NavBar folder and fix styling

--- frontend/src/components/NavBar/pages/Login.jsx
+++ frontend/src/components/NavBar/pages/Login.jsx
@@ -2,66 +2,46 @@ import React, { Fragment, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
 
-function Register( {setAuth} ) {
-  const [inputs, setInputs] = useState({
-    name: "",
+function Login({ setAuth }) {
+
+  const [ inputs, setInputs ] = useState({
     email: "",
     password: ""
   });
 
   const navigate = useNavigate();
 
-  const {name, email, password} = inputs;
+  const { email, password } = inputs;
 
   //Function to handle onChange for inputs
-  const handleOnChange = e => {
+  const handleOnChange = (e) => {
     setInputs({...inputs, [e.target.name] : e.target.value})
-  };
+  }
 
-  // Function to handle onSubmit for signup
+  // Function to handle onSubmit for login
   const handleOnSubmitForm = async (e) => {
-    console.log("Form submitted")
+    console.log("Login clicked")
     e.preventDefault();
-
-    const body = {name, email, password};
-
+    
     try {
-      const response = await axios.post("http://localhost:3000/auth/register", body)
+      const body = {email, password};
+      const response = await axios.post("http://localhost:3000/auth/login", body)
       console.log(response.data);
 
-      //Save token in localStorge
+      //Save token to localStorage
       localStorage.setItem("token", response.data.token);
-
       setAuth(true);
-
       navigate("/");
     } catch (error) {
       console.error(error.message);
     }
   }
 
+
   return (
     <Fragment>
-      <h1 className="text-5xl py-4">Register</h1>
-
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleOnSubmitForm}>
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="name"
-          >
-            Name
-          </label> 
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="name"
-            name="name"
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={e => handleOnChange(e)}
-          />
-        </div>
+      <h1 className="text-5xl py-4 mx-auto text-center">Login</h1>
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mx-auto max-w-lg justify-self-center" onSubmit={handleOnSubmitForm}>
 
         <div className="mb-4">
           <label
@@ -73,8 +53,8 @@ function Register( {setAuth} ) {
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="email"
-            type="email"
             name="email"
+            type="email"
             placeholder="Email"
             value={email}
             onChange={e => handleOnChange(e)}
@@ -91,8 +71,8 @@ function Register( {setAuth} ) {
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
-            type="password"
             name="password"
+            type="password"
             placeholder="Password"
             value={password}
             onChange={e => handleOnChange(e)}
@@ -103,7 +83,7 @@ function Register( {setAuth} ) {
           className="border-black border-2 rounded-2xl py-2 px-4"
           type="submit"
         >
-          Sign Up
+          Login
         </button>
       </form>
       
@@ -111,4 +91,4 @@ function Register( {setAuth} ) {
   );
 }
 
-export default Register;
+export default Login;
