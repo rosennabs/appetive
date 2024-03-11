5102650b14e167e9c04ca9d0693aa3f80b668eb6
hisbanshiraz <hisbanbardai@gmail.com>
2024-03-10 19:32:08 -0600

    Add route to fetch all recipes from the database

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
 const db = require("../db/connection");
 
-exports.modules = router;
+router.get("/", async (req, res) => {
+  try {
+    const allRecipes = await db.query(`SELECT * FROM recipes;`);
+    res.status(200).json(allRecipes.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+module.exports = router;
