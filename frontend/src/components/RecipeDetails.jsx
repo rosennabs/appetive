9c47969d6100430b04520bb92d0e5680a3a5c08e
hisbanshiraz <hisbanbardai@gmail.com>
2024-03-11 16:38:56 -0600

    Add dynamic 'id' prop and useState hook to manage recipe details state

---
+++ frontend/src/components/RecipeDetails.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const RecipeDetails = function ({ id }) {
+  const [recipeDetails, setRecipeDetails] = useState(null);
+
+  return <h1>Recipe Details Page</h1>;
+};
+
+export default RecipeDetails;
