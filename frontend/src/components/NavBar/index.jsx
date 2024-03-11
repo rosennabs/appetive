e956dfc8d416359d7205433a63db9093ab341048
Mia Dinh <miadinh38@gmail.com>
2024-03-11 02:01:02 -0400

    Fix error to prevent the request from being made when the user is not logged in

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,17 +1,13 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Login from "../Login";
-import Register from "../Regsiter";
+import { Nav, NavLink, NavMenu, NavBtn, Bars, NavBtnLink, } from "./NavBarElements";
 import axios from "axios";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import About from "./pages/About";
+import { FaAngleDoubleDown } from "react-icons/fa";
 
-import {
-  Nav,
-  NavLink,
-  NavMenu,
-  NavBtn,
-  Bars,
-  NavBtnLink,
-} from "./NavBarElements";
+
 
 function NavBar() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -29,8 +25,15 @@ function NavBar() {
   // Keep user login status and set 'setAuth' to 'true' upon page refresh
   const isAuth = async() => {
     try {
+      const token = localStorage.token;
+      // Check if the token exists
+      if (!token) {
+        setIsAuthenticated(false); // Set isAuthenticated to false if token doesn't exist
+        return; // Exit the function early
+      }
+      
       const headers = {
-        token: localStorage.token
+        token: token
       }
       const response = await axios.get("http://localhost:3000/auth/is-verify", {headers})
       // console.log(response.data); //true
@@ -53,21 +56,26 @@ function NavBar() {
         <NavMenu>
           <NavLink to="/">HOME</NavLink>
           <NavLink to="/about">ABOUT US</NavLink>
-          <NavLink to="/browse-recipes">BROWSE RECIPES</NavLink>
-          <NavLink to="/dashboard">DASHBOARD</NavLink>
+          <NavLink to="/browse-recipes" className="dropdown-menu">
+            BROWSE RECIPES
+            <FaAngleDoubleDown className="ml-1" />
+          </NavLink>
+          <NavLink to="/my-profile">
+            MY PROFLE
+            <FaAngleDoubleDown className="ml-1" />
+          </NavLink>
         </NavMenu>
 
         <NavBtn>
           {!isAuthenticated ? (
             <>
-              <NavLink to="/login">LOGIN</NavLink>
-
-              <NavLink to="/register">REGISTER</NavLink>
+              <NavBtnLink to="/login">LOGIN</NavBtnLink>
+              <NavBtnLink to="/register">SIGN UP</NavBtnLink>
             </>
           ) : (
             <>
-              <p>Logged In</p>
-              <NavBtn onClick={e => handleLogout(e)}>LOGOUT</NavBtn>
+              <p>Logged In</p><br></br>
+              <NavBtnLink onClick={e => handleLogout(e)}>LOGOUT</NavBtnLink>
             </>
           )}
         </NavBtn>
@@ -77,7 +85,11 @@ function NavBar() {
       <Routes>
         <Route path="/login" element={<Login setAuth={setAuth} />} />
         <Route path="/register" element={<Register setAuth={setAuth} />} />
+        <Route path="/about" element={<About />}/>
       </Routes>
+
+      <img src="../../Images/header-img.jpg" alt="Header Image" />
+
     </>
   );
 }
