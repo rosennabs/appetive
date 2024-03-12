b1b4851ece47c2f2761947de03145686b7f728e8
maddie <lee.maddie@gmail.com>
2024-03-12 14:11:56 -0400

    Create getCuisineByName query

--- backend/db/queries/recipes_helpers.js
+++ backend/db/queries/recipes_helpers.js
@@ -1,8 +1,27 @@
 const db = require("../connection");
 
 // lookup cuisine and return ID
+const getCuisineByName = async function (cuisineName) {
+  try {
+    const queryString = `SELECT id FROM cuisines WHERE name LIKE $1;`;
+    const queryParams = [`%${cuisineName}%`];
+    const cuisine = await db.query(queryString, queryParams);
+
+    if (cuisine.rows.length === 0) {
+      return { message: "Cuisine not found" };
+    }
+
+    return cuisine.rows[0];
+  } catch (error) {
+    console.error("Error in getCuisineByName:", error.message);
+    throw error;
+  }
+};
+
 // lookup diet and return ID
 // lookup meal_type and return ID
 // lookup ingredient and return ID
 
-module.exports = {};
+module.exports = {
+  getCuisineByName
+};
