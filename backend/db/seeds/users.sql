136fddb03afaf272d6f987e65b4fde9197fedc12
Mia Dinh <miadinh38@gmail.com>
2024-03-12 23:31:53 -0400

    Rename schema and seed files

--- backend/db/seeds/users.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO
-  users (name, email, password)
-VALUES
-  ('Henry', 'henry@gmail.com', 'henry123');
-
-INSERT INTO
-  users (name, email, password)
-VALUES
-  ('Hisban', 'test@gmail.com', '12345');
