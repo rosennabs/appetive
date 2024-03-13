a69b6e9c87019413bf6c650fe4542ea5be2f831e
hisbanshiraz <hisbanbardai@gmail.com>
2024-03-13 12:20:03 -0600

    Add all types of cuisines to match values with external API

--- backend/db/seeds/03_cuisines.sql
+++ backend/db/seeds/03_cuisines.sql
@@ -1,9 +1,30 @@
 INSERT INTO
   cuisines (name)
 VALUES
-  ('French');
-
-INSERT INTO
-  cuisines (name)
-VALUES
-  ('German');
+  ('African'),
+  ('Asian'),
+  ('American'),
+  ('British'),
+  ('Cajun'),
+  ('Caribbean'),
+  ('Chinese'),
+  ('Eastern European'),
+  ('European'),
+  ('French'),
+  ('German'),
+  ('Greek'),
+  ('Indian'),
+  ('Irish'),
+  ('Italian'),
+  ('Japanese'),
+  ('Jewish'),
+  ('Korean'),
+  ('Latin American'),
+  ('Mediterranean'),
+  ('Mexican'),
+  ('Middle Eastern'),
+  ('Nordic'),
+  ('Southern'),
+  ('Spanish'),
+  ('Thai'),
+  ('Vietnamese');
