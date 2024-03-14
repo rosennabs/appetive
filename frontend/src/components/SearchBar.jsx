273dd7ace6502dd762055d806bc13db735573e9d
Rose Francis <rosennabs@gmail.com>
2024-03-14 01:00:44 -0600

    -Handle user's input. -Make axios call to api to fetch autocomplete data

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,19 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Formik, Form, Field } from 'formik';
 import { AppDataContext } from '../contexts/AppDataContext';
+import axios from "axios";
+import apiKey from "../config";
 
 export default function SearchBar() {
   const [query, setQuery] = useState(''); //State variable to store user's recipe input
   const [suggestions, setSuggestions] = useState([]); //To store suggested possible recipe names
 
-  
+
   //Set state as user inputs characters
   const handleInputChange = (event) => {
     const userInput = event.target.value;
+    //console.log("User's input: ", userInput);
     setQuery(userInput);
   }
 
-  
+  //Fetch autocomplete suggestions base on user's input
+  useEffect(() => {
+    const fetchAutoComplete = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.spoonacular.com/recipes/autocomplete?apiKey=${apiKey}&number=2&query=${query}`
+        );
+
+        console.log("Autocomplete recipe names: ", response.data);
+        setSuggestions(response.data);
+
+  } catch (error) {
+        console.error("Error fetching autocomplete recipe names: ", error);
+      }
+    };
+    fetchAutoComplete();
+  }, []); 
+
 
   return (
     <Formik
@@ -21,21 +41,21 @@ export default function SearchBar() {
       onSubmit={(values, actions) => {
         // Handle form submission
 
-         console.log(values);
+         //console.log("values: ", values);
         
         actions.setSubmitting(false);
       }}
       
     >
-      {({ handleSubmit, handleChange, values}) => (
+      {({ handleSubmit}) => (
         <Form onSubmit={handleSubmit}>
           <div className="flex justify-center items-center bg-white border border-gray-300 rounded-md shadow-lg p-16">
           
           <input
             type="text"
             name="query"
-            value={values.query}
-            onChange={handleChange}
+            value={query}
+            onChange={handleInputChange}
             placeholder="Search recipes"
             autoComplete='off'
             aria-autocomplete='list'
