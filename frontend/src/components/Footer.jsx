fe7fa660984a02120dbf77c0cbb4935367f9cc4e
Mia Dinh <miadinh38@gmail.com>
2024-03-17 03:29:52 -0400

    Update text color

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-black text-white py-4">
+    <footer className="bg-black text-darker-white py-4">
       <div className="container mx-auto px-4">
         <p className="text-center">© 2024 Appetive. All rights reserved.</p>
       </div>
