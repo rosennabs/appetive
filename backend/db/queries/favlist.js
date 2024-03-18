369c2d32c672209c33d42cb6b15fb408dfc23f76
maddie <lee.maddie@gmail.com>
2024-03-18 10:28:23 -0400

    Initialize file

---
+++ backend/db/queries/favlist.js
@@ -0,0 +1,7 @@
+const db = require("../connection");
+
+// GET all favs for user from users_recipes table and get information for each recipe
+
+// query if a logged in user has marked a recipe as fav. if TRUE return TRUE; if FALSE return FALSE
+
+// update user marking a recipe as fav from TRUE to FALSE or FALSE to TRUE
