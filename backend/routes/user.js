f9c3d4b61eac1c2a01e0449ee4e83b1ea6c742d9
maddie <lee.maddie@gmail.com>
2024-03-18 21:03:20 -0400

    Create route to READ/BROWSE all user favs

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,5 +5,21 @@ const {
   checkIfFav,
   toggleIsFav
 } = require("../db/queries/favlist");
+const jwtDecoder = require("../utils/jwtDecoder");
+
+// READ - GET - display all user favs
+router.get("/:id", async (req, res) => {
+  try {
+    const { user } = await jwtDecoder(req.body); // req should contain token from localStorage
+    const userFavs = await getUserFavs(user);
+    const displayFavs = await displayUserFavs(userFavs);
+    res.status(200).json(displayFavs);
+  } catch (error) {
+    console.error("Error in api/user/:id route:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// UPDATE - POST - toggle fav
 
 module.exports = router;
