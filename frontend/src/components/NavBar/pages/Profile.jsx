70a54fea47ea0e943e3146b36710c886a3a66dd1
maddie <lee.maddie@gmail.com>
2024-03-19 21:18:08 -0400

    Display FavList only if user is logged in

--- frontend/src/components/NavBar/pages/Profile.jsx
+++ frontend/src/components/NavBar/pages/Profile.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import FavList from "../../FavList";
+import { Link } from "react-router-dom";
 
 export default function Profile () {
+
+  const loggedIn = localStorage.token;
+
   return (
-    <div>
-      <h1 className="text-center">User Profile</h1>
+    <div className="my-5">
+    { loggedIn ? (
+      <div>
+      <h1 className="text-center">User Profile for {loggedIn}</h1>
       <FavList />
     </div>
+    ) : (
+      <p className="text-center text-xl">
+        You must <Link to="/login" className="font-bold text-brown-light hover:underline">log in</Link> to see this content.
+      </p>
+    )}
+  </div>
   )
 };
