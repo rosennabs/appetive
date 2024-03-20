d70249895d8d2d6a8bdd92175423331cdb87bdb2
maddie <lee.maddie@gmail.com>
2024-03-20 16:27:34 -0400

    Use useEffect to get username from token

--- frontend/src/components/NavBar/pages/Profile.jsx
+++ frontend/src/components/NavBar/pages/Profile.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React , { useEffect, useState, } from "react";
 import FavList from "../../FavList";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function Profile () {
 
   const loggedIn = localStorage.token;
+  const [ username, setUsername ] = useState('');
+
+  useEffect(() => {
+    const getUsername = async (token) => {
+      try {
+        const response = await axios.post(`http://localhost:8080/api/user/`, token);
+        console.log(response);
+        setUsername(response.data);
+      } catch (error) {
+        console.error("Error fetching username:", error);
+      };
+    }
+    getUsername(loggedIn);
+  }, []);
 
   return (
     <div className="my-5">
     { loggedIn ? (
       <div>
-      <h1 className="text-center">User Profile</h1>
+      <h1 className="text-center">{username}'s Profile</h1>
       <FavList />
     </div>
     ) : (
