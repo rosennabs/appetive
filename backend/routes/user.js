4be3f87baa87a391adf661e3b92e9e687b11fd2b
maddie <lee.maddie@gmail.com>
2024-03-19 21:47:40 -0400

    Add route to get username

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,23 +5,36 @@ const {
   checkIfFav,
   toggleIsFav
 } = require("../db/queries/favlist");
+const { getUserNameById } = require("../db/queries/recipes_helpers");
 const jwtDecoder = require("../utils/jwtDecoder");
 
-// READ - GET - display all user favs
-router.get("/:id/favs", async (req, res) => {
+// GET - get username
+router.get(":/id", async (req, res) => {
   try {
-    const { user } = await jwtDecoder(req.body); // req should contain token from localStorage
-    const userFavs = await getUserFavs(user);
-    const displayFavs = await displayUserFavs(userFavs);
-    res.status(200).json(displayFavs);
+    const { user } = await jwtDecoder(req.body);
+    const username = getUserNameById(user);
+    res.status(200).json(username);
   } catch (error) {
     console.error("Error in api/user/:id route:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
+// READ - GET - display all user favs
+router.get("/:id/fav", async (req, res) => {
+  try {
+    const { user } = await jwtDecoder(req.body); // req should contain token from localStorage
+    const userFavs = await getUserFavs(user);
+    const displayFavs = await displayUserFavs(userFavs);
+    res.status(200).json(displayFavs);
+  } catch (error) {
+    console.error("Error in api/user/:id/fav route:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 // UPDATE - POST - toggle fav
-router.post("/:id/favs", async (req, res) => {
+router.post("/:id/fav", async (req, res) => {
   try {
     // req should contain token from localStorage AND recipe id
     const { user } = await jwtDecoder(req.body.token);
@@ -31,7 +44,7 @@ router.post("/:id/favs", async (req, res) => {
     res.status(204).send(toggle);
 
   } catch (error) {
-    console.error("Error in api/user/:id route:", error.message);
+    console.error("Error in api/user/:id/fav route:", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
