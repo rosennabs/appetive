0fc383e262e922763716e9f49c17a3681896000c
Mia Dinh <miadinh38@gmail.com>
2024-03-21 02:39:37 -0400

    Update CounterAttempt component to set initial value of isToggled to match the user_recipe data

--- frontend/src/components/CounterAttempt.jsx
+++ frontend/src/components/CounterAttempt.jsx
@@ -1,40 +1,56 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useContext, useState, useEffect } from "react";
 import axios from "axios";
 
-function CounterAttempt() {
-  const [toggled, setToggled] = useState(false);
+function CounterAttempt({ recipeId }) {
+  const [isToggled, setToggled] = useState(false);
+  const jwtToken = localStorage.token;
 
-  const user_id = localStorage.getItem("token");
-  const { id } = useParams();
+  useEffect(() => {
+    const fetchUserRecipeData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/api/recipes/userRecipeData/${recipeId}`, {
+          headers: {'token': jwtToken }
+        });
+        console.log("Response: ", response.data);
+        const userRecipeData = response.data;
+        if (userRecipeData && userRecipeData.has_tried) {
+          setToggled(userRecipeData.has_tried); // Set initial value based on fetched data
+        }
+      } catch (error) {
+        console.error('Error fetching user recipe data:', error.message);
+      }
+    };
 
-  const handleToggleBtn = async() => {
-    console.log("Toggled");
-    setToggled((prev) => !prev);
+    fetchUserRecipeData();
+  }, []);
+
+
+const handleToggleButton = async () => {
+    console.log("Toggled: ", recipeId)
     try {
-      const res = await axios.post(`http://localhost:8080/api/recipes/${id}`, {user_id})
-      console.log("Response from toggle button", res);
+      const res = await axios.post(`http://localhost:8080/api/recipes/${recipeId}`, null, {
+        headers: {'token': jwtToken }
+      });
+      const results = res.data;
+      console.log("Response from toggle button: ", results);
+      setToggled(prev => !prev)
+      return res.data;
     } catch (error) {
-      console.error("Error from handleToggleBtn", error.message);
+      console.error(error.message);
       throw error;
     }
-  };
+}
 
   return (
     <>
-      {/* Hidden input to pass user_id to the server */}
-      <div>
-        <input name="user_id" type="hidden" value={user_id} />
-      </div>
-
       <label className="inline-flex items-center cursor-pointer">
-        <span className="ms-3 mr-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+        <span className="mr-3 text-sm font-medium text-black">
           Have you tried this recipe?
         </span>
         <input
           type="checkbox"
-          checked={toggled}
-          onChange={handleToggleBtn}
+          checked={isToggled}
+          onChange={handleToggleButton}
           className="sr-only peer"
         ></input>
         <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-yellow"></div>
