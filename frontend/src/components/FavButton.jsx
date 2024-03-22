5a7e4fe3f4942aea6b4062133bdb7e4582d68ffa
maddie <lee.maddie@gmail.com>
2024-03-22 14:26:25 -0400

    Write click handler for fav toggle

--- frontend/src/components/FavButton.jsx
+++ frontend/src/components/FavButton.jsx
@@ -1,30 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { FaHeart } from "react-icons/fa";
+import axios from "axios";
 
-export default function FavButton () {
+export default function FavButton (props) {
   const [ isFav, setIsFav ] = useState(false);
   const userToken = localStorage.token;
+  const recipe_id = props.recipe_id;
 
-  useEffect(() => {
-  const handleFavClick = async (token, recipe_id, fav) => {
+  const handleFavClick = async (token, recipe_id) => {
     try {
-      const response = await axios.post(`http://localhost:8080/api/user/fav`, { token });
-      const allFavs = response.data;
-
-      setFavs(allFavs);
+      const response = await axios.post(`http://localhost:8080/api/user/recipe/${recipe_id}/fav`, {
+        token,
+        recipe_id
+      });
+      
+      setIsFav(response.data);
     } catch (error) {
       console.error("Error fetching favs:", error);
     }
   };
-    fetchFavs(userToken);
-  }, []);
 
   // useEffect to check if fav on load/on click
   // function to send if fav on click
   // state for fav state
   return (
     <div>
-      <section className="flex border border-black h-10 px-8 justify-center bg-yellow w-[200px]">
+      <section className="flex border border-black h-10 px-8 justify-center bg-yellow w-[200px]" onClick={() => {handleFavClick(userToken, recipe_id)}}>
         <p className="flex items-center">
           <FaHeart />
           <button className="ml-2">Favourite</button>
