08667a650fac5b11900e27483f43f1251e5e6d5a
Mia Dinh <miadinh38@gmail.com>
2024-03-23 06:43:13 -0400

    Move all review routes to new file

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -5,10 +5,8 @@ const db = require("../db/connection");
 const {
   getRecipes,
   getRecipeById,
-  getReviewsByRecipeId,
   addRecipe,
   getRecipesBySearchQuery,
-  addReview,
   toggleHasTried,
   updateCounter,
   getUserRecipeData,
@@ -71,46 +69,6 @@ router.get("/:id", async (req, res) => {
       // Recipe found
       res.status(200).json(recipe);
     }
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server error");
-  }
-});
-
-router.get("/:id/reviews", async (req, res) => {
-  try {
-    const recipeId = req.params.id;
-    if (!recipeId) {
-      return res.status(400).json("Invalid recipe ID");
-    }
-    const reviews = await getReviewsByRecipeId(recipeId);
-
-    if ("message" in reviews) {
-      // No reviews found for the recipe
-      res.status(404).json(reviews);
-    } else {
-      // Reviews found, return the array
-      res.status(200).json(reviews);
-    }
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Server error");
-  }
-});
-
-router.post("/:id/reviews", authorization, async (req, res) => {
-  const recipeId = req.params.id;
-  const newReview = req.body;
-  newReview.user_id = req.user;
-  try {
-    const result = await addReview(
-      recipeId,
-      newReview.rating,
-      newReview.review,
-      newReview.user_id
-    );
-    console.log("Review added", result.rows);
-    res.status(201).send(newReview);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server error");
