db8e35a12def174d6723dfa1d09662cc9a1c0785
hisbanshiraz <hisbanbardai@gmail.com>
2024-03-24 11:10:37 -0600

    Remove all code related to routing from NavBar component

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,49 +1,24 @@
 import React, { useState, useEffect } from "react";
 import SearchBar from "../SearchBar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {
-  Nav,
   NavLink,
   NavMenu,
   NavBtn,
   Bars,
   NavBtnLink,
   ImgBtnLink,
+  Nav,
 } from "./NavBarElements";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
 import { FaCaretDown, FaSearch, FaHome } from "react-icons/fa";
-import RecipeForm from "../RecipeForm";
-import useAuthentication from "../../hooks/useAuthentication";
-import AuthenticationError from "../AuthenticationError";
 import axios from "axios";
+import useAuthentication from "../../hooks/useAuthentication";
 
-function NavBar({ toggleSearchBar, showSearchBar, setSelected }) {
+function NavBar({ toggleSearchBar, showSearchBar, setSelected, username }) {
   const { isAuthenticated, setAuth, handleLogout } = useAuthentication();
-  const [username, setUsername] = useState("");
-  const token = localStorage.token;
-
-  useEffect(() => {
-    const getUsername = async (token) => {
-      try {
-        const response = await axios.post(`http://localhost:8080/api/user/`, {
-          token,
-        });
-        console.log(response);
-        setUsername(response.data);
-      } catch (error) {
-        console.error("Error fetching username:", error);
-      }
-    };
-    getUsername(token);
-  }, []);
 
   return (
     <>
       <Bars />
-
       <Nav className="fixed top-0">
         <NavMenu>
           <NavLink to="/">
@@ -59,7 +34,6 @@ function NavBar({ toggleSearchBar, showSearchBar, setSelected }) {
             <FaCaretDown className="ml-1" />
           </NavLink>
         </NavMenu>
-
         <NavBtn>
           {!isAuthenticated ? (
             <>
@@ -82,7 +56,6 @@ function NavBar({ toggleSearchBar, showSearchBar, setSelected }) {
           )}
         </NavBtn>
       </Nav>
-
       <div>
         <img
           src={require("../../Images/header.png")}
@@ -91,31 +64,11 @@ function NavBar({ toggleSearchBar, showSearchBar, setSelected }) {
         />
         <ImgBtnLink to="/add-recipe">MAKE YOUR RECIPE</ImgBtnLink>
       </div>
-
-           {/* Search bar */}
-        
-          <div>
-            {showSearchBar && <SearchBar />}
-          </div>
-      
-      <ImgBtnLink to="/add-recipe" onClick={() => setSelected(null)}>MAKE YOUR RECIPE</ImgBtnLink>
-
-      <Routes>
-        <Route path="/my-profile" element={<Profile username={username} />} />
-        <Route path="/login" element={<Login setAuth={setAuth} />} />
-        <Route path="/register" element={<Register setAuth={setAuth} />} />
-        <Route path="/about" element={<About />} />
-        <Route
-          path="/add-recipe"
-          element={
-            isAuthenticated ? (
-              <RecipeForm setAuth={setAuth} />
-            ) : (
-              <AuthenticationError />
-            )
-          }
-        />
-      </Routes>
+      {/* Search bar */}
+      <div>{showSearchBar && <SearchBar />}</div>
+      <ImgBtnLink to="/add-recipe" onClick={() => setSelected(null)}>
+        MAKE YOUR RECIPE
+      </ImgBtnLink>
     </>
   );
 }
