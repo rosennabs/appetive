bd384e701a426df5d7434f7fa7159607fa63daa3
hisbanshiraz <hisbanbardai@gmail.com>
2024-03-24 15:31:33 -0600

    Set isAuthenticated initial state based on if token exists or not

--- frontend/src/hooks/useAuthentication.js
+++ frontend/src/hooks/useAuthentication.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const useAuthentication = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.token);
   const [username, setUsername] = useState("");
 
   const setAuth = (boolean) => {
