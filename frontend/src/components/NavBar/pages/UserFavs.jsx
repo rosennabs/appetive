5086fc1ed01a0b7ecb578d3cf3f45bb734671b5c
maddie <lee.maddie@gmail.com>
2024-03-27 13:05:13 -0400

    Adjust padding to centre authentication error

--- frontend/src/components/NavBar/pages/UserFavs.jsx
+++ frontend/src/components/NavBar/pages/UserFavs.jsx
@@ -7,10 +7,10 @@ function UserFavs( {username} ) {
   const { isAuthenticated } = useAuthentication();
 
   return (
-    <div className="mb-28">
+    <div className="mb-28 pt-12">
       {isAuthenticated ? (
         <div>
-          <h1 className="text-4xl pt-12 pb-0 font-bold mx-auto text-center">
+          <h1 className="text-4xl py-0 font-bold mx-auto text-center">
             {username}'s Favourite Recipes
           </h1>
           <FavList />
